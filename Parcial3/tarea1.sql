85a73c0762effc72c31d5026865b21e552d81aad
AngelSebastianGarciaSosa <78859205+AngelSebastianGarciaSosa@users.noreply.github.com>
2021-05-07 14:52:02 -0500

    tarea 1 parcial3

---
+++ Parcial3/tarea1.sql
@@ -0,0 +1,16 @@
+create database tarea1;
+use tarea1
+
+create table usuarios
+(
+    nombrealumno varchar(50),
+    NumDeControl varchar(50),
+    telefono varchar(50),
+    email varchar(50),
+    carrera varchar(50),
+    password  varchar (30)
+    primary key NumDeControl
+
+);
+insert into usuarios (nombrealumno,NumDeControl, telefono,email,carrera,password)
+values('Angel Sebastian Garcia Sosa','18100174','(867)7523656','chevis.123@hotmail.com','ISC' );
