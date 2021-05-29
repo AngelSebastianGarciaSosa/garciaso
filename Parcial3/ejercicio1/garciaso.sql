ad612f34826cafd3fa53a9d07a790fdf797a9b8c
AngelSebastianGarciaSosa <78859205+AngelSebastianGarciaSosa@users.noreply.github.com>
2021-05-29 01:48:00 -0500

    comit

--- Parcial3/ejercicio1/garciaso.sql
+++ Parcial3/ejercicio1/garciaso.sql
@@ -8,9 +8,9 @@ create table usuarios
     telefono varchar(50),
     email varchar(50),
     carrera varchar(50),
-    password  varchar (30)
+    pass  varchar (30)
     primary key NumDeControl
 
 );
-insert into usuarios (nombrealumno,NumDeControl, telefono,email,carrera,password)
+insert into usuarios (nombrealumno,NumDeControl, telsefono,email,carrera,pass)
 values('Angel Sebastian Garcia Sosa','18100174','(867)7523656','chevis.123@hotmail.com','ISC' ,'123');
