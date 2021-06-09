ec867e09b1b0203052cd126e4c960db94ef76b7b
AngelSebastianGarciaSosa <78859205+AngelSebastianGarciaSosa@users.noreply.github.com>
2021-06-08 20:17:14 -0500

    comit

--- Parcial3/loginbasededatos/SQL/LoginBD.sql
+++ Parcial3/loginbasededatos/SQL/LoginBD.sql
@@ -8,6 +8,14 @@ create table usuarios
     Contraseña varchar(200),
     primary key (id)
 );
-
+create table alumnos
+(
+    primary key id int Auto_Increment,
+    NumControl varchar(150),
+    NombreAlumno varchar(150),
+    Email varchar(150),
+    Carrera varchar(150),
+    primary key (id)
+);
 insert into usuarios(Usuario,Email,Contraseña)
 values ('sebastian','Sebastian','$2y$10$8W8eupTlaUaO.5RJafYJReF6soSzuqsPi/4oW8sZPObAx99FiUwdO');
